e0bb3abd3c9d7e4867e21ff75864799f660df013
osaruyi5 <80843322+osaruyi5@users.noreply.github.com>
2021-04-11 14:55:32 +0100

    Delete app.js

--- app.js
+++
@@ -1,6 +0,0 @@
-const btnToggler = document.getElementById('btn');
-const navLink = document.querySelector('.nav-links');
-
-btnToggler.addEventListener('click', function () {
- navLink.classList.toggle('active');
-});
